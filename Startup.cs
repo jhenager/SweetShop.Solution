3e558982116fc901ef05de6e8a00ef617c33ff27
James Henager <jameschenager@gmail.com>
2020-08-14 08:48:08 -0700

    add boilerplate to program, startup, csproj

---
+++ Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SweetShop.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace SweetShop
+{
+  public class Startup
+  {
+    public Startup(IHostingEnvironment env)
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json"); 
+    Configuration = builder.Build();
+}
+
+    public IConfigurationRoot Configuration { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+    services.AddMvc();
+
+    services.AddEntityFrameworkMySql()
+        .AddDbContext<SweetShopContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
+    services.AddIdentity<ApplicationUser, IdentityRole> ()
+        .AddEntityFrameworkStores<SweetShopContext>()
+        .AddDefaultTokenProviders();
+
+        services.Configure<IdentityOptions>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredUniqueChars = 0;
+    });
+
+          
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseStaticFiles();
+
+      app.UseDeveloperExceptionPage();
+
+      app.UseAuthentication();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+          name: "default",
+          template: "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Something went wrong!");
+      });
+      
+    }
+  }
+}
