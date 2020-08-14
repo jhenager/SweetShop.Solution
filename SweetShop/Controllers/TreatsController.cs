cd28be9356c2e964e5f63e5fedebdd18d1454f2b
James Henager <jameschenager@gmail.com>
2020-08-14 14:38:02 -0700

    add edit view to treats

--- SweetShop/Controllers/TreatsController.cs
+++ SweetShop/Controllers/TreatsController.cs
@@ -30,9 +30,13 @@ namespace SweetShop.Controllers
       return View();
     }
     [HttpPost]
-    public ActionResult Create(Treat treat)
+    public ActionResult Create(Treat treat, int FlavorId)
     {
         _db.Treats.Add(treat);
+        if (FlavorId != 0)
+          {
+            _db.TreatFlavor.Add(new TreatFlavor() {FlavorId = FlavorId, TreatId = treat.TreatId});
+          }
         _db.SaveChanges();
         return RedirectToAction("Index");
     }
@@ -44,6 +48,7 @@ namespace SweetShop.Controllers
         .FirstOrDefault(treat => treat.TreatId == id);
       return View(thisTreat);
     }
+    [Authorize]
     public ActionResult Edit(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
@@ -54,6 +59,21 @@ namespace SweetShop.Controllers
     public ActionResult Edit(Treat treat)
     {
         _db.Entry(treat).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    [Authorize]
+    public ActionResult Delete(int id)
+    {
+        var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+        return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+        _db.Treats.Remove(thisTreat);
         _db.SaveChanges();
         return RedirectToAction("Index");
     }
